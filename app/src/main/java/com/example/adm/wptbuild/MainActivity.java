d50beebf5c48ad64a74b4aaaa3b559d98bb750db
Thiago Antonio <taoalu@gmail.com>
2018-04-11 22:48:46 -0300

    Correção na MainActivity

--- app/src/main/java/com/example/adm/wptbuild/MainActivity.java
+++ app/src/main/java/com/example/adm/wptbuild/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int sp, ep;
+
     EditText lvlText;
     TextView pointsView;
 
@@ -18,18 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int sp;
-        int ep;
-
-        lvlText = (EditText) findViewById(R.id.levelText);
-        pointsView = (TextView) findViewById(R.id.pointsView);
+        lvlText = findViewById(R.id.levelText);
+        pointsView = findViewById(R.id.pointsView);
 
         String points = lvlText.getText().toString();
-        if(points !=""){
+
+        if (!points.equals("")) {
             int pt = Integer.parseInt(points);
-            int point = ((pt-9)*5) + ((pt-79)*2) + ((pt-89)*3) +25;
-            sp = ((pt-9)/2)+2;
-            ep = (pt-59)/2;
+            int point = ((pt - 9) * 5) + ((pt - 79) * 2) + ((pt - 89) * 3) + 25;
+            sp = ((pt - 9) / 2) + 2;
+            ep = (pt - 59) / 2;
             pointsView.setText("Avaiable points: " + point);
         }
 
